a13ff7f3077ff58f20f9b0f3e05dc6cfb6832b18
jgoley <jgoley@gmail.com>
2014-12-10 00:01:50 -0500

    Refactors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,43 +9,22 @@
 
  */
 
-var wikiURL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&grnlimit=1&prop=extracts&exintro=';
+var options = [
+	'format=json',
+	'action=query',
+	'generator=random',
+	'grnnamespace=0',
+	'grnlimit=1',
+	'prop=extracts',
+	'exintro='
+],
+	base_url = 'https://en.wikipedia.org/w/api.php?',
+	wikiAPI_URL = buildLink(base_url, options);
 
-// Get JSON with ajax request
-function request(){
-	return $.ajax({ 
-		url: wikiURL,
-		type: 'GET',
-		dataType: 'jsonp',
-	});
-}
-
-/* -------------------------------------------------------------
-Make request and when done call function to display data 
-Uses underscore to get the first random object because the data 
-modeling is not great. Ideally the 'pages' object should be an array. 
-------------------------------------------------------------- */
-function getData() {
-	request().done(function(data){
-		displayData(data.query.pages[_.keys(data.query.pages)[0]]);
-	});
-}
-
-// Adds data/Updates DOM
-function displayData(article){
-	$('.title').html(article.title);
-	$('.intro').html(getFirstP(article.extract));
-}
-
-// Gets first paragraph from extract
-function getFirstP(text){
-	return text.split("</p>")[0]+"</p>";
-}
-
-getData();
+getData(wikiAPI_URL);
 
 $(function(){
 	$('.get-another').on('click', function(){
-		getData();
+		getData(wikiAPI_URL);
 	});
 });
