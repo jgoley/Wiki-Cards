13fbe5190e25f878cfb109284e1d2b89e03ab593
jgoley <jgoley@gmail.com>
2014-12-10 15:26:26 -0500

    Switches to dummy data for styling and such

--- app/scripts/fn.js
+++ app/scripts/fn.js
@@ -1,3 +1,5 @@
+var dummy = {"warnings":{"main":{"*":"Unrecognized parameter: 'undefined'"},"query":{"*":"Formatting of continuation data will be changing soon. To continue using the current formatting, use the 'rawcontinue' parameter. To begin using the new format, pass an empty string for 'continue' in the initial query."}},"query":{"pages":{"26696317":{"pageid":26696317,"ns":0,"title":"Cover meter","extract":"<p>A <b>cover meter</b> is an instrument to locate rebars and measure the exact concrete cover. Rebar detectors are less sophisticated devices that can only locate metallic objects below the surface. Due to the cost-effective design, the pulse-induction method is one of the most commonly used solutions.</p>\n<p></p>\n"}}}}
+
 function buildLink(base_url, opts){
 	var linkParams; 
 	_.each(opts, function(opt){
@@ -21,12 +23,14 @@ Uses underscore to get the first random object because the data
 modeling is not great. Ideally the 'pages' object should be an array. 
 ------------------------------------------------------------- */
 function getData(url) {
-	request(url).done(function(data){
-		displayData(data.query.pages[_.keys(data.query.pages)[0]]);
-	});
+	// request(url).done(function(data){
+	// 	displayData(data.query.pages[_.keys(data.query.pages)[0]]);
+	// });
+	displayData(dummy.query.pages[_.keys(dummy.query.pages)[0]]);
+
 }
 
-// Adds data/Updates DOM
+// Updates DOM
 function displayData(article){
 	var title = article.title;
 	$('.title').html(title);
