337a3561665476dad7a87f1f7d92ecf8e745b2f7
jgoley <jgoley@gmail.com>
2014-12-09 23:45:37 -0500

    Implements data refresh with ajax call

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,51 @@
+/*
+    ___        ________   ________     
+   |\  \      |\   ____\ |\   __  \    
+   \ \  \     \ \  \___| \ \  \|\  \   
+ __ \ \  \     \ \  \  ___\ \  \\\  \  
+|\  \\_\  \  ___\ \  \|\  \\ \  \\\  \ 
+\ \________\|\__\\ \_______\\ \_______\
+ \|________|\|__| \|_______| \|_______|
 
-function getData(){
+ */
+
+var wikiURL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&grnlimit=1&prop=extracts&exintro=';
+
+// Get JSON with ajax request
+function request(){
 	return $.ajax({ 
-		url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&grnlimit=1&prop=extracts&exintro=',
+		url: wikiURL,
 		type: 'GET',
 		dataType: 'jsonp',
 	});
 }
 
+/* -------------------------------------------------------------
+Make request and when done call function to display data 
+Uses underscore to get the first random object because the data 
+modeling is not great. Ideally the 'pages' object should be an array. 
+------------------------------------------------------------- */
+function getData() {
+	request().done(function(data){
+		displayData(data.query.pages[_.keys(data.query.pages)[0]]);
+	});
+}
 
-getData().done(function(data){
-	displayData(data.query.pages[_.keys(data.query.pages)[0]]);
-});
-
+// Adds data/Updates DOM
 function displayData(article){
 	$('.title').html(article.title);
 	$('.intro').html(getFirstP(article.extract));
 }
 
+// Gets first paragraph from extract
 function getFirstP(text){
 	return text.split("</p>")[0]+"</p>";
 }
+
+getData();
+
+$(function(){
+	$('.get-another').on('click', function(){
+		getData();
+	});
+});
